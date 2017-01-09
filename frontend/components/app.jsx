467ec79c87e29f9525319bb53bb97bea663ff454
Alex Sherman <asherman.ca@gmail.com>
2017-01-09 15:55:00 -0800

    auth 90% complete

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router';
+import GreetingContainer from './greeting/greeting_container';
+//
+//
+// const App = ({ children }) => (
+//   <div>
+//     <h1>Bench BnB</h1>
+//     { children }
+//   </div>
+// );
+//
+// export default App;
 
 const App = ({ children }) => (
   <div>
-    <h1>Bench BnB</h1>
-    { children }
+    <header>
+      <Link to="/" className="header-link">
+        <h1>Bench BnB</h1>
+      </Link>
+      <GreetingContainer />
+    </header>
+    {children}
   </div>
 );
 
--- frontend/snackchat.jsx
+++ frontend/snackchat.jsx
@@ -4,21 +4,17 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 //window testing
-import { login, signup, logout } from './util/session_api_util.js';
+import { signup, logout } from './util/session_api_util.js';
+import { login } from './actions/session_actions.js';
 window.login = login;
 window.signup = signup;
 window.logout = logout;
-window.store = configureStore();
+// window.store = configureStore();
 //end window testing
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+  window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
 });
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const root = document.getElementById('root');
-//     ReactDOM.render(<h1>Welcome to SnackChat</h1>, root);
-// });
