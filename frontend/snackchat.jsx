6c8a06461347713db08bf555a93ce17d51ea632d
Alex Sherman <asherman.ca@gmail.com>
2017-01-09 15:01:04 -0800

    router added

--- frontend/snackchat.jsx
+++ frontend/snackchat.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, signup, logout } from './util/session_api_util.js';
+import Root from './components/root';
 import configureStore from './store/store';
 
+//window testing
+import { login, signup, logout } from './util/session_api_util.js';
 window.login = login;
 window.signup = signup;
 window.logout = logout;
 window.store = configureStore();
+//end window testing
 
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to SnackChat</h1>, root);
+  const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={ store }/>, root);
 });
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const root = document.getElementById('root');
+//     ReactDOM.render(<h1>Welcome to SnackChat</h1>, root);
+// });
