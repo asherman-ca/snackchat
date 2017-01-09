cfac88bf27b2db47d749a84394bfbeb134cbf011
Alex Sherman <asherman.ca@gmail.com>
2017-01-09 14:48:11 -0800

    auth

--- frontend/snackchat.jsx
+++ frontend/snackchat.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { login, signup, logout } from './util/session_api_util.js';
+import configureStore from './store/store';
 
 window.login = login;
 window.signup = signup;
 window.logout = logout;
+window.store = configureStore();
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
