869cc6a891faba3b1356091a356d91bfca54179f
Alex Sherman <asherman.ca@gmail.com>
2017-01-10 15:55:42 -0800

    lodash fix

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -9,15 +9,25 @@ import App from './app';
 // import SessionFormContainer from './session_form/session_form_container.js';
 import Welcome from './welcome';
 
-const Root = ({ store }) => (
+
+
+const Root = ({ store }) => {
+
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  };
+
+  return (
   <Provider store={ store }>
     <Router history={ hashHistory }>
-
       <Route path="/" component={ Welcome } />
-      
 
     </Router>
   </Provider>
-);
+  );
+};
 
 export default Root;
