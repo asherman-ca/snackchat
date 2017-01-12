7312b0d2b9de214bf0f41141cc121fd9b1d5dc6d
Alex Sherman <asherman.ca@gmail.com>
2017-01-12 15:43:56 -0800

    app component content

--- frontend/components/app/app.jsx
+++ frontend/components/app/app.jsx
@@ -22,14 +22,28 @@ class App extends React.Component {
 
   render(){
     return (
-      <div>
-        <header>
-          <a onClick={ () => this.props.logout()}>
-            <span>Logout</span>
-          </a>
-        </header>
-        <div className='browse'>
-          {this.props.children}
+      <div className='browse-sidebars'>
+        <div className='browse-content'>
+          <div className='navbar'>
+            <div className='navbar-logo'>
+              <Link to='/'><img src="http://res.cloudinary.com/devwor0u0/image/upload/v1484180645/SnackChat_hwfh0w.png"/></Link>
+            </div>
+
+            <div className='navbar-header'>
+              <h1><Link to='/'>SnackChat</Link></h1><p>share your snacks!</p>
+            </div>
+
+            <div className='navbar-buttons'>
+            <div className='navbar-button'>
+              <a onClick={ () => this.props.logout()}>
+                <span>Logout</span>
+              </a>
+            </div>
+          </div>
+        </div>
+          <div className='browse'>
+            {this.props.children}
+          </div>
         </div>
       </div>
     );
