f7f65029c6273f824819bb5ab97645b62bc019aa
Alex Sherman <asherman.ca@gmail.com>
2017-01-12 14:28:21 -0800

    sidebars added

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -25,39 +25,41 @@ class Splash extends React.Component {
     }
   }
 
-  render(){
+  render() {
     return (
-        <div>
-        <div className='navbar'>
-          <div className='navbar-logo'>
-            <Link to='/'><img src="http://res.cloudinary.com/devwor0u0/image/upload/v1484180645/SnackChat_hwfh0w.png"/></Link>
+      <div className='splash-sidebars'>
+        <div className='splash-content'>
+          <div className='navbar'>
+            <div className='navbar-logo'>
+              <Link to='/'><img src="http://res.cloudinary.com/devwor0u0/image/upload/v1484180645/SnackChat_hwfh0w.png"/></Link>
+            </div>
+
+            <div className='navbar-header'>
+              <h1><Link to='/'>SnackChat</Link></h1><p>share your snacks!</p>
+            </div>
+
+            <div className='navbar-buttons'>
+              <div className='navbar-button btn-login'>
+                <Link to='/login'>
+                  <span>Login</span>
+                </Link>
+              </div>
+              <div className='navbar-button btn-signup'>
+                <Link to='/signup'>
+                  <span>Signup</span>
+                </Link>
+              </div>
+              <div className='navbar-button btn-demo'>
+                <a onClick={ () => this.props.login(demoUser) }>
+                  <span>Demo</span>
+                </a>
+              </div>
+            </div>
           </div>
 
-          <div className='navbar-header'>
-            <h1><Link to='/'>SnackChat</Link></h1><p>share your snacks!</p>
+          <div className='splash'>
+            { this.props.children }
           </div>
-
-          <div className='navbar-buttons'>
-            <div className='navbar-button btn-login'>
-              <Link to='/login'>
-                <span>Login</span>
-              </Link>
-            </div>
-            <div className='navbar-button btn-signup'>
-              <Link to='/signup'>
-                <span>Signup</span>
-              </Link>
-            </div>
-            <div className='navbar-button btn-demo'>
-              <a onClick={ () => this.props.login(demoUser) }>
-                <span>Demo</span>
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className='splash'>
-          { this.props.children }
         </div>
       </div>
     );
