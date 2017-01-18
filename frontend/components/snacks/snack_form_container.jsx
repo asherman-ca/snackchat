b043e08be539292cb7e64a284f74dcc7557892da
Alex Sherman <asherman.ca@gmail.com>
2017-01-17 16:35:01 -0800

    final Tuesday push

--- frontend/components/snacks/snack_form_container.jsx
+++ frontend/components/snacks/snack_form_container.jsx
@@ -2,8 +2,9 @@ import { connect } from 'react-redux';
 import SnackForm from './snack_form.jsx';
 import { addSnack } from '../../actions/snack_actions';
 
-const mapStateToProps = ({ errors }) => ({
-  errors: errors
+const mapStateToProps = ({ errors, currentUser }) => ({
+  errors: errors,
+  user: currentUser
 });
 
 const mapDispatchToProps = (dispatch) => {
