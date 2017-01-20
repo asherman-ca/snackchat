658831e87c6e99cfd7a022e007920744be635dda
Alex Sherman <asherman.ca@gmail.com>
2017-01-20 10:09:38 -0800

    backend functions

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -2,4 +2,11 @@ class Snack < ActiveRecord::Base
   validates :name, presence: true
   has_many :ratings, dependent: :destroy
   belongs_to :user
+
+  def average_rating
+    all_ratings = self.ratings
+    return 5 if all_ratings.empty?
+    all_ratings.inject(0) { |sum, r| sum + r.rating } / all_ratings.length
+  end
+
 end
--- app/views/api/snacks/index.json.jbuilder
+++ app/views/api/snacks/index.json.jbuilder
@@ -9,8 +9,9 @@
       json.id snack.id
       json.name snack.name
       json.picture_url snack.picture_url
-      json.user_id snack.user_id
       json.description snack.description
+      json.average_rating snack.average_rating
+      json.user_id snack.user_id
       json.user_name snack.user.username
   end
 end
