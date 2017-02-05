8b69bc3ba497146c301b3d70a225f0e17f3cd43d
Alex Sherman <asherman.ca@gmail.com>
2017-02-04 21:57:32 -0800

    username cell size and margin

--- frontend/components/snacks/snacks_index.jsx
+++ frontend/components/snacks/snacks_index.jsx
@@ -28,7 +28,7 @@ class SnacksIndex extends React.Component {
         <div className="snack-index">
           <div className="snack-index-container">
             <div className="snack-index-user">
-              <h1>{snacks[0].user_name} Snacks</h1>
+              <h1>{snacks[0].user_name}'s Snacks</h1>
             </div>
             {snacks.map(snack => <SnackIndexItem key={snack.id} snack={snack} />)}
           </div>
